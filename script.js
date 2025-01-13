e7a3543a1246397a32b7be2d870293fac50f8005
Yama Jiang <yamajiang.cs@gmail.com>
2025-01-13 13:54:47 -0500

    favicon update

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function toggleMenu(){
     icon.classList.toggle("open");
 
 }
+
+function toggleMenu1(){
+    const menu= document.querySelector(".menu-links1");
+    const icon= document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
+
