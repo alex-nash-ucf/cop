9a419b925c3881f3a46d1352c5038f4cf84ae991
Nathan Davis <na980947@ucf.edu>
2025-01-16 17:11:50 -0500

    Added DeleteContacts

---
+++ LAMPAPI/DeleteContacts.php
@@ -0,0 +1,41 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$first_name = $inData["firstName"];
+	$last_name = $inData["lastName"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		// ? is to avoid SQL injection attacks. Act as placeholders to later be binded.
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE UserId = ? AND FirstName = ? AND LastName = ?");
+		$stmt->bind_param("iss", $userId, $first_name, $last_name);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
